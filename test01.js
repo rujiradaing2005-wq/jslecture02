04d42d2ba3f7b75e1a0fdaa7f72d22c505cf8bd6
rujiradaa <rujiradaing2005@gmail.com>
2025-09-28 12:29:36 +0700

    สร้างไฟล์ 01, 02, 03 พื้นฐาน js

---
+++ test01.js
@@ -0,0 +1,43 @@
+//single line comment
+
+
+/*Multi line Comment
+💫
+💢
+*/
+console.log("Hello...")
+
+
+console.log("AAAAA")
+console.log('BBBBB')
+console.log(`CCCCC`)
+
+//number
+console.log(11111)
+console.log(15.426879)
+
+//Boolean
+console.log(true)
+console.log(false)
+
+//Null
+console.log(null)
+
+//Object เขียนอยู่ใน { } และมีรูปแบบเป็น key : value
+console.log({
+    name : 'Sombat',
+    age : 20,
+    isStudent : true,
+    address : {
+        province : 'Nonthaburi',
+        contry : 'thailand'
+    },
+    food : ['KFC', 'Pizza', 'Burger']
+})
+
+//Array เขียนอยู่ใน [ ] square brackets มีได้หลายข้อมูลคั่นด้วย , comma
+console.log([1,2,3,4,5])
+console.log([111, 'Somjai', true, 555, {NAME : 'Sombat', age : 20}])
+
+let x
+console.log(x)
---
+++ test02.js
@@ -0,0 +1,15 @@
+var dti01 = 'aaa' 
+let dti02 = 'bbb' 
+const dti03 = 'ccc' 
+
+dti01 ='111'
+dti02 ='222'
+//dti03 ='333'
+{
+    var dti04 = 'ddd'
+    let dti05 = 'eee'
+    const dti06 = 'fff'
+    console.log(dti01, dti02, dti03, dti04, dti05, dti06)
+}
+
+console.log(dti01, dti02, dti03, dti04)
